e8adb55012c494fe660e3f1c3870d0a11f6d62bb
HARISH KIZHAKKE VALAPPIL <harishkv619@gmail.com>
2024-09-22 06:01:40 +0530

    [changed first commit]

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 let sum=0, count
 count=parseInt(prompt("Enter anumber between 0 and 1 million: "))
-if(count<0 && count>1000000)
+if(count<0 || count>1000000)
 {
     console.log("Invalid number");
 }
 else{
     for(let i=0;i<=count;i++){
-    sum+=count
+    sum+=i
     }
     document.write("Sum of numbers up to "+ count + " is " +sum)
 }
